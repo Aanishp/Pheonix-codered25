d9e53f16686b291dbdfa857d2fdbe476afc88b7b
Aanishp <137623512+Aanishp@users.noreply.github.com>
2025-01-10 04:08:21 +0530

    main

--- main.py
+++ main.py
@@ -18,4 +18,28 @@ def welcome_page():
 def services_page():
     return render_template('services.html')
 
+@app.route('/roadmap')
+def roadmap_page():
+    return render_template('roadmap.html')
+
+@app.route('/front-end')
+def front_end_page():
+    return render_template('front-end.html')
+
+@app.route('/back-end')
+def back_end_page():
+    return render_template('back-end.html')
+
+@app.route('/cybersecurity')
+def cybersecurity_page():
+    return render_template('cybersecurity.html')
+
+@app.route('/Dev-Ops')
+def dev_Ops_page():
+    return render_template('Dev-Ops.html')
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
 app.run(debug=True)
